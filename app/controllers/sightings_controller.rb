9cff082984a25a3e9306f7afe40e8cfcf45501f2
turpp <white.tripp@icloud.com>
2021-01-13 19:02:56 -0600

    commit tree 6e8f9910da6d7ca38494d6ef2b1df6bac4a1e203

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -3,4 +3,8 @@ class SightingsController < ApplicationController
         @sighting = Sighting.find(params[:id])
         render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
     end
+
+    def index
+        sightings=Sighting.all
+        render json: SightingsSerializer.new(sightings).to_serialized_json
 end
